9fe6ff5610321fdc5b3b412c6cb389ba0eefa6b2
Ryan Walmsley <ryanteck@gmail.com>
2018-12-04 22:10:36 +0000

    Updated to support latest RAK Chip firmware.

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@
 *
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* Last Updated 2018-12-04
+* Last Updated 2018-12-04-1
 */
 
 enum Channels {
