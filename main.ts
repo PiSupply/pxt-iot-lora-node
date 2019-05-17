dc4d7a17fc0bd38f0d3d18ddaeafe1e11cd6e42a
Ryan Walmsley <ryanteck@gmail.com>
2019-05-17 13:33:27 +0100

    Fix region change

--- main.ts
+++ main.ts
@@ -223,7 +223,13 @@ namespace IotLoRaNode {
          */
         
 
-
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(300)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        serial.readLine()
+        serial.readLine()
+        serial.readLine()
+        basic.pause(75)
         serial.writeString("at+band=" + regionsList[regionVal] + "\r\n");
         serial.readUntil(serial.delimiters(Delimiters.NewLine))
         serial.readUntil(serial.delimiters(Delimiters.NewLine))
