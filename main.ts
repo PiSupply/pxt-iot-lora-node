fbf8c71743bad981a759568d5b643166132d492f
Ryan Walmsley <ryanteck@gmail.com>
2019-07-10 12:47:20 +0100

    #30 Improvement

--- main.ts
+++ main.ts
@@ -235,7 +235,6 @@ namespace IotLoRaNode {
         serial.writeString("at+band=" + regionsList[regionVal] + "\r\n");
        serial.readUntil(serial.delimiters(Delimiters.NewLine))
         basic.showIcon(IconNames.Diamond)
-        serial.readUntil(serial.delimiters(Delimiters.NewLine)) //getting stuck here
         pins.digitalWritePin(DigitalPin.P16, 1)
         basic.pause(300)
         pins.digitalWritePin(DigitalPin.P16, 0)
