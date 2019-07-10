d607196ba0bcb9312e36b95b0db2aa37d0df4b33
Ryan Walmsley <ryanteck@gmail.com>
2019-07-10 12:52:03 +0100

    Fully fixed #30. It also seems to be able to handle the LoRa Region block in code if at the top.

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@
 *
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* Last Updated 2019-06-15-1242
+* Last Updated 2019-07-10-1251
 */
 
 enum Channels {
