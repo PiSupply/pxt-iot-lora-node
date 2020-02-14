6d7531298de85fd0099dd01f77fb3efc5f9b8f5c
Ryan Walmsley <ryan@pi-supply.com>
2020-02-14 18:54:43 +0000

    Updates.

--- main.ts
+++ main.ts
@@ -507,8 +507,9 @@ namespace IotLoRaNode {
         serial.writeString("at+rd_adc=" + pinNum + "\r\n");
 
         let value = serial.readBuffer(6)
-        value = value.substr(-4)
-        basic.showString(value.toString())
+        let value2 = value.toString()
+        value2 = value2.substr(-4)
+        //basic.showString(value.toString())
         
 
         basic.showIcon(IconNames.Yes)
